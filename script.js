815d79832c8c59e63ca658201f5632d222204486
popelkin-roman <mymaythe23@gmail.com>
2024-07-25 23:55:48 +0500

    Add tooltip

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const drawChart = (data) => {
     const w = 1000;
     const h = 500;
     const padding = 50;
+    let tileColor = '';
 
     const color = d3.scaleOrdinal(data.children.map(d => d.name), d3.schemeTableau10);
 
@@ -38,67 +39,37 @@ const drawChart = (data) => {
         .text(d => `${d.ancestors().reverse().map(d => d.data.name).join(".")}\n${format(d.value)}`);
 
     leaf.append("rect")
-      // .attr("id", d => (d.leafUid = DOM.uid("leaf")).id)
+      .attr("class", "tile")
+      .attr("data-name", d => d.data.name)
+      .attr("data-category", d => d.data.category)
+      .attr("data-value", d => d.data.value)
       .attr("fill", d => { while (d.depth > 1) d = d.parent; return color(d.data.name); })
       .attr("fill-opacity", 0.6)
       .attr("width", d => d.x1 - d.x0)
-      .attr("height", d => d.y1 - d.y0);
-    // const color = d3.scaleOrdinal(d3.schemeDark2);
-    // const minYear = new Date(0).setFullYear(d3.min(data, d => d.Year - 1));
-    // const maxYear = new Date(0).setFullYear(d3.max(data, d => d.Year + 1));
-    // const minTime = new Date("2000-01-01T00:"+ d3.min(data, d => d.Time));
-    // const maxTime = new Date("2000-01-01T00:" + d3.max(data, d => d.Time));
+      .attr("height", d => d.y1 - d.y0)
+      .on("mouseover", (e, d) => {
+            tileColor = e.target.style.fill;
+            e.target.style.fill = "#aaa";
+            const tooltip = d3.select("#tooltip")
+                .attr("data-name", d.data.name)
+                .attr("data-category", d.data.category)
+                .attr("data-value", d.data.value)
+                .style("visibility", "visible")
+                .style("transform", `translateX(${e.clientX}px) translateY(${e.clientY}px)`);
 
-    // const scaleYear = d3.scaleTime()
-    //     .domain([minYear, maxYear])
-    //     .range([padding, w - padding]);
-    // const scaleTime = d3.scaleTime()
-    //     .domain([minTime, maxTime])
-    //     .range([padding, h - padding]);
-        
-    // svg.selectAll("circle")
-    //     .data(data)
-    //     .enter()
-    //     .append("circle")
-    //     .attr("cx", (d) => scaleYear(new Date(0).setFullYear(d.Year)) )
-    //     .attr("cy", (d) => scaleTime(new Date("2000-01-01T00:" + d.Time)))
-    //     .attr("r", 5)
-    //     .attr("class", "dot")
-    //     .attr("data-xvalue", d => d.Year)
-    //     .attr("data-yvalue", d => new Date("2000-01-01T00:" + d.Time))
-    //     .style("fill", d=> color(d.Doping !== ''))
-    //     .on("mouseover", (e, d) => {
-    //         e.target.style.fill = "#aaa";
-    //         const tooltip = d3.select("#tooltip")
-    //             .attr("data-year", d.Year)
-    //             .attr("data-time", d.Time)
-    //             .style("visibility", "visible")
-    //             .style("transform", `translateX(${e.clientX}px) translateY(${e.clientY}px)`)
-    //         tooltip.append("div")
-    //             .text(d.Year)
-    //         tooltip.append("div")
-    //             .text(d.Time)
-    //     })
-    //     .on("mouseout", (e,d) => {
-    //         e.target.style.fill = color(d.Doping !== '');
-    //         d3.select("#tooltip")
-    //             .style("visibility", "hidden")
-    //             .text("");
+            tooltip.append("div")
+                .text(d.data.name)
+            tooltip.append("div")
+                .text(d.data.value)
+        })
+        .on("mouseout", (e,d) => {
+            e.target.style.fill = tileColor;
+            d3.select("#tooltip")
+                .style("visibility", "hidden")
+                .text("");
             
-    //     })
+        })
     
-    // const xAxis = d3.axisBottom(scaleYear);
-    // const yAxis = d3.axisLeft(scaleTime)
-    //     .tickFormat(d3.timeFormat("%M:%S"));
-    // const xAxisLine = svg.append("g")
-    //     .attr("id", "x-axis")
-    //     .attr("transform", `translate(${0}, ${h - padding})`)
-    //     .call(xAxis);
-
-    // const yAxisLine = svg.append("g")
-    //     .attr("id", "y-axis")
-    //     .attr("transform", `translate(${padding}, ${0})`)
-    //     .call(yAxis);
     
     // const legendContainer = svg.append('g').attr('id', 'legend');
 
@@ -133,8 +104,8 @@ const drawChart = (data) => {
     //         }
     //       });
 
-    // d3.select(".scatterplot")
-    //     .append("div")
-    //     .attr("id", "tooltip")
-    //     .style("visibility", "hidden");
+    d3.select(".treemap")
+        .append("div")
+        .attr("id", "tooltip")
+        .style("visibility", "hidden");
 }
